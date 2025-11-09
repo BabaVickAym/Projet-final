c8796961a3fce920ce672a01b78fed130b1fac06
Didier <hassoumoualoo@gmail.com>
2025-11-09 22:32:02 +0100

    ajout de la recherche d'un projet par id

--- main.py
+++ main.py
@@ -57,10 +57,13 @@ def list_projects():
 
 
 # Endpoint 3: GET /projects/{id} (Obtenir les détails d'un projet spécifique)
-@app.get("/projects/{project_id}", response_model=Project)
-def get_project(project_id: str):
+@app.get("/projects/{id}", response_model=Project)
+def get_project(id: str):
+    # Lire db.json à chaque requête pour garantir des données fraîches
+    data = load_data()
+
     # Chercher le projet dans la liste
-    project_data = next((p for p in data["projects"] if p["id"] == project_id), None)
+    project_data = next((p for p in data["projects"] if p["id"] == id), None)
 
     if project_data is None:
         raise HTTPException(status_code=404, detail="Projet non trouvé")
