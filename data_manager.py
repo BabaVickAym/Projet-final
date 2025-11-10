6436d3ecece4d339387ddf6595be3b7b182bd643
Ravel Pessy <ravelpessy@gmail.com>
2025-11-10 09:37:42 +0100

    modification data_man init

--- data_manager.py
+++ data_manager.py
@@ -26,5 +26,10 @@ def save_data(data: Dict[str, List[Dict[str, Any]]]):
 
 
 # Initialisation du fichier si nécessaire
-if not load_data().get("projects"):
+data = load_data()
+
+if not data or "projects" not in data or not data["projects"]:
     save_data({"projects": []})
+    print("✅ Fichier initialisé avec succès : la liste des projets est vide pour le moment.")
+else:
+    print("✅ Les données existent déjà : aucune initialisation nécessaire.")
