6253ef2c60b9499c754fbae418612c8f4cd12ed2
DelliaBiloa <biloabrenda0105@gmail.com>
2025-11-10 08:32:57 +0100

    Règlement des conflits

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-<<<<<<< HEAD
-# main.py
-from fastapi import FastAPI
-from features.filter_by_course import router as course_router
 
-app = FastAPI(title="ProjetAPI", description="API pour la gestion des projets étudiants.")
-
-# Inclusion du routeur
-app.include_router(course_router)
-
-# Optionnel : un endpoint racine simple
-@app.get("/")
-async def read_root():
-    return {"message": "Bienvenue sur ProjetAPI"}
-=======
 from fastapi import FastAPI, HTTPException
 from data_manager import load_data, save_data
 from models import Project, ProjectIn, GradeUpdate
@@ -144,4 +130,4 @@ def get_projects_by_course(course_name: str):
         pass
 
     return filtered_projects
->>>>>>> 0fdb9031657c1eef093b6c9d986663f452744a55
+
